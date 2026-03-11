68b41db984fe3a978f4bb9755969e3521b903b00
Timalsinarabin <timalsinarabin11@gmail.com>
2026-03-11 11:53:12 +0000

    feat: add login

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,15 @@ Rails.application.routes.draw do
   # Get /main
   root "main#index"
 
-  # Get /profile
-  get "/profile", to: "profile#index"
+  # login for sign in
+  get "/sign_in", to: "login#index", as: :sign_in
+  post "/sign_in", to: "login#profile"
 
   # Register a new user
   get "/sign_up", to: "registrations#new", as: :sign_up
   post "/sign_up", to: "registrations#create"
+
+  get "/dashboard", to: "dashboard#index"
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
