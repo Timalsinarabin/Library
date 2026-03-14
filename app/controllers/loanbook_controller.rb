13b3d32ff42a1f4f2050d85c93e0e6a615834b1e
Timalsinarabin <timalsinarabin11@gmail.com>
2026-03-14 12:04:50 +0000

    feat: display loan books

--- app/controllers/loanbook_controller.rb
+++ app/controllers/loanbook_controller.rb
@@ -1,18 +1,24 @@
 class LoanbookController < ApplicationController
   def index
     @loan = Loan.new
+    @books = LibBook.all
   end
   def create
-    @loan = Loan.new(loan_params)
-    if @loan.save
-      redirect_to loan_book_path, notice: "Sucessfully loaned book"
+    book = LibBook.find_by(bookname: loan_params[:bookname])
+    if book
+      @loan = Loan.new(loan_params)
+      if @loan.save
+        redirect_to loan_book_path, notice: "Sucessfully loaned book"
+      else
+        redirect_to loan_book_path, alert: "Unsucessfull to loan book"
+      end
     else
-      redirect_to loan_book_path, alert: "Unsucessfull to loan book"
+      redirect_to loan_book_path, alert: "Book does not exists"
     end
   end
-end
 
-private
-def loan_params
-  params.require(:loan).permit(:std_id, :bookname)
+  private
+  def loan_params
+    params.require(:loan).permit(:std_id, :bookname)
+  end
 end
